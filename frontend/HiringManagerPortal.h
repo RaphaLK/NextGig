9543d6b498668eb10575dc52f0818742238f2559
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2025-05-28 16:40:22 -0700

    HiringManagerPortal UI update

--- frontend/HiringManagerPortal.h
+++ frontend/HiringManagerPortal.h
@@ -11,36 +11,38 @@
 #include <QCheckBox>
 #include "../src/models/User.h"
 #include "UserManager.h"
-class HiringManagerPortal : public QWidget {
+class HiringManagerPortal : public QWidget
+{
   Q_OBJECT
 public:
   explicit HiringManagerPortal(QWidget *parent = nullptr);
-  QWidget* renderHiringManagerPortal();
-  void setCurrentUser(User* user);
+  QWidget *renderHiringManagerPortal();
+  void setCurrentUser(User *user);
 
 private:
   // Tab creation methods
-  QWidget* createDashboardTab();
-  QWidget* createProfileTab();
-  QWidget* createPostJobTab();
-  QWidget* createMessagesTab();
-  QWidget* createRatingsTab();
-  
+  QWidget *createDashboardTab();
+  QWidget *createProfileTab();
+  QWidget *createPostJobTab();
+  QWidget *createMessagesTab();
+  QWidget *createRatingsTab();
+
   // Helper methods
   void updateProfileInfo();
   void fetchProfileFromFirebase();
-
+  void loadProposalsForHiringManager();
+  void updateProposalStatus(const QString &jobId, const QString &freelancerId, const QString &status);
   // UI elements that need to be accessed from multiple methods
-  QListWidget* postedJobsList;
+  QListWidget *incomingPrpsls;
   QLabel *nameLabel;
   QLabel *emailLabel;
-  QTextEdit* descriptionTextEdit;
-  QLabel* companyNameLabel;
-  QTextEdit* companyDescriptionTextEdit;
-  QListWidget* accomplishmentsList;  
+  QTextEdit *descriptionTextEdit;
+  QLabel *companyNameLabel;
+  QTextEdit *companyDescriptionTextEdit;
+  QListWidget *accomplishmentsList;
 
   // User data
-  User* currentUser;
+  User *currentUser;
 
 signals:
   void returnToHomeRequested(); // LOGOUT
